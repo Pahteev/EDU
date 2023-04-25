b4fc9512ee9b964b341553c97a562bc705b0396d
Pahteev <a.pahteev@gmail.com>
2023-04-26 01:38:27 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,13 +1,12 @@
 import requests
 
 article_id = ['Лондон', 'Шереметьево', 'Череповец']
-
-for i in article_id:
+for article in article_id:
     try:
         url_template = 'https://wttr.in/{}?n?M?q?T&lang=ru'
-        url = url_template.format(i)
+        url = url_template.format(article)
         response = requests.get(url)
         response.raise_for_status()
         print(response.text)
     except:
-        print(f'Информация о погоде в городе {i} временно недоступен\n')
+        print(f'Информация о погоде в городе {article} временно недоступен\n')
